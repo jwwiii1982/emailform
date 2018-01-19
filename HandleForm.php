62886236f95d7dff7a21c67749a65fce36642641
jwwiii1982 <jwwiii1982@gmail.com>
2018-01-18 21:34:51 -0500

    Add files via upload

---
+++ HandleForm.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Form Results</title>
+</head>
+<body>
+
+<?php
+
+print "Your first name is $FirstName.<br>\n";
+print "Your last name is $LastName.<br>\n";
+print "Your E-mail address is $Email.<br>\n";
+print "This is what you had to say:<br>\n $Comments<br>\n";
+
+
+</body>
+</html>
